8e1b5cbe1009cc85d06db063f87333a3487ea412
Brainy0403 <godwinbernard9@gmail.com>
2024-07-27 21:04:22 +0100

    NavbarElement

---
+++ main.js
@@ -0,0 +1,15 @@
+// navbar section
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () =>{
+    navbar.classList.toggle('active');
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+}
+window.onscroll = () => {
+    search.classList.remove('active');
+    cart.classList.remove('active');
+    user.classList.remove('active');
+    navbar.classList.remove('active');
+}
